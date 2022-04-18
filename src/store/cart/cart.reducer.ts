14e59efd962776375d51873b9fd911ce0ba0d686
Billy Cao <Billycaos123@gmail.com>
2022-04-18 19:33:24 +1000

    adding files

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,32 @@
+import { CartItem } from "./cart.types";
+import { AnyAction } from "redux";
+import { setCartItems, setIsCartOpen } from "./cart.action";
+
+const CART_INITIAL_STATE: CartState = {
+  isCartOpen: false,
+  cartItems: [],
+};
+
+export type CartState = {
+  readonly isCartOpen: boolean;
+  readonly cartItems: CartItem[];
+};
+
+export const cartReducer = (
+  state = CART_INITIAL_STATE,
+  action: AnyAction
+): CartState => {
+  if (setIsCartOpen.match(action)) {
+    return {
+      ...state,
+      isCartOpen: action.payload,
+    };
+  }
+  if (setCartItems.match(action)) {
+    return {
+      ...state,
+      cartItems: action.payload,
+    };
+  }
+  return state;
+};
